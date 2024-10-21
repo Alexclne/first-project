c2b572a48aeffae78e1d68c8f6826edf0f8462c0
Alexclne <alexnegru2000@gmail.com>
2024-10-21 23:49:27 +0200

    Creazine Auth

--- note/urls.py
+++ note/urls.py
@@ -1,10 +1,12 @@
 # note/urls.py
 from django.urls import path
-from .views import note , createNoteView , updateNoteView , removeNoteView #importazione metodi da views.py
+from .views import note , create_note_view ,  update_note_view , remove_note_view  , registration #importazione metodi da views.py
 
 urlpatterns = [
     path('', note, name='note'),  # Questa è la vista principale per l'app 'note'
-    path('form/' , createNoteView , name= 'create_note'),
-    path('update/<int:f_id>/', updateNoteView , name = 'update_note' ),
-    path('delete/<int:f_id>/', removeNoteView , name = 'remove_note' ),
+    path('form/' , create_note_view , name= 'create_note'),
+    path('update/<int:f_id>/', update_note_view , name = 'update_note' ),
+    path('delete/<int:f_id>/', remove_note_view , name = 'remove_note' ),
+    #AUTH PATH
+    path('registration/', registration , name='registration' ),
 ]
--- note/views.py
+++ note/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render , redirect
 from django.shortcuts import get_list_or_404, get_object_or_404
 from .models import Note #Dal modulo models.py importo il modello Note
 from .forms import NoteForm
-
+from django.contrib.auth.forms import UserCreationForm
 
 
 
@@ -17,7 +17,7 @@ def note(request):
 
 
 #CREATE
-def createNoteView(request):
+def create_note_view(request):
     
     if request.method == "POST":#Se l'utente ha iniviato il modolo(se esegue una richiesta di tipo post)
         form = NoteForm(request.POST)#Si crea un Nuovo NoteForm con tutti i dati POST
@@ -29,7 +29,7 @@ def createNoteView(request):
     return render(request, "note/form.html", {"form": form})
 
 #UPDATE
-def updateNoteView(request , f_id):
+def update_note_view(request , f_id):
     obj = get_object_or_404(Note, id=f_id)
     #Il metodo get_object_or_404 concede di restituire l'oggetto richiesto solo e solamente se esiste. Nel caso l'oggetto non esistesse si genera un errore 404
     if request.method == "POST":
@@ -45,10 +45,23 @@ def updateNoteView(request , f_id):
 
 
 #DELETE
-def removeNoteView(request , f_id):
+def remove_note_view(request , f_id):
     obj = Note.objects.get(id=f_id)
     if request.method == "POST":
         obj.delete()
         return redirect("note")
         
     return render(request, 'note/delete.html', {"obj":obj})
+
+#VIEW FOR AUTH
+def registration(request):
+    if request.method == "POST":
+        user = UserCreationForm(request.POST)
+        if user.is_valid():
+            user.save()
+            return redirect('note')
+    else:
+        user=UserCreationForm()
+    return render(request , "auth/registration.html" , {"user":user})
+#PROSEGUIRE DA QUI.
+
