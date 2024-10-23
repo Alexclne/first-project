ce30ddfae44a02aa7763844d1495edd898d75b77
Alexclne <alexnegru2000@gmail.com>
2024-10-23 19:08:30 +0200

    interfaccia note singolo user

--- note/models.py
+++ note/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User #modello fornito da Django che consente la gestione degli utenti 
 
 # Create your models here.
 #CREO E IMPOSTO IN QUESTO CASO DUE PAMETRI DEL MIO MODELLO 'Note'
@@ -6,10 +7,9 @@ class Note(models.Model):
     nome = models.CharField(max_length=40)
     description = models.CharField(max_length=2000)
     created_at = models.DateTimeField(auto_now_add= True)
+    utente= models.ForeignKey(User, on_delete=models.CASCADE)#User con questa relazione è il modello figlio. Eseguito import da dja
 
-    #In questo modo avremo una rappresentazione esplicita sul Django Administration
-    #Ci verrà ritornato il nome effettivo della nota scritta come titolo
-    def __str__(self):
+    def __str__(self):    #In questo modo avremo una rappresentazione esplicita sul Django Administration.Ci verrà ritornato il nome effettivo della nota scritta come titolo
         return self.nome
     
 
